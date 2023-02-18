b928f576bb6a0ccbefdbdc6377eb1587f8eef40e
Aman Kapoor <akalasklo@gmail.com>
2023-02-18 11:49:10 +0530

    Add files via upload

---
+++ mediapipe2.py
@@ -0,0 +1,119 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle 
+
+# Make Detections
+# cap = cv2.VideoCapture(0)
+# with mp_pose.Pose(min_detection_confidence = 0.5,min_tracking_confidence=0.5)as pose:
+#     while cap.isOpened():
+#         ret, frame = cap.read()
+#         # Recolor image to RGB
+#         image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+#         image.flags.writeable = False
+
+#         # Make detection 
+#         results = pose.process(image)
+
+#         # Recolor back to BGR
+#         image.flags.writeable =True
+#         image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+
+#         # Reder detections
+#         mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(244,119,64),thickness=1,circle_radius = 2),mp_drawing.DrawingSpec(color=(245,65,232),thickness=1,circle_radius = 2))
+
+#         cv2.imshow('Mediapipe Feed', image)
+        
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
+            
+#     cap.release()
+#     cv2.destroyAllWindows()
+
+# Determining Joints
+cap = cv2.VideoCapture(0)
+# Curl Counter Variable
+counter = 0
+stage = None
+
+with mp_pose.Pose(min_detection_confidence = 0.5,min_tracking_confidence=0.5)as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        # Make detection 
+        results = pose.process(image)
+
+        # Recolor back to BGR
+        image.flags.writeable =True
+        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+
+        # Extract Landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            angle = calculate_angle(shoulder,elbow,wrist)
+            # visualization
+            cv2.putText(image,str(angle),tuple(np.multiply(elbow,[640,480]).astype(int)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2,cv2.LINE_AA)
+
+            # curl counter logic
+            if angle>160:
+                stage = "down"
+            if angle<30 and stage=="down":
+                stage = 'up'
+                counter+=1
+                print(counter)
+            # print(landmarks)
+            # print(calculate_angle(shoulder,elbow,wrist))
+        except:
+            pass
+         # Render curl counter
+        # Setup status box
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        # Rep data
+        cv2.putText(image,"REPS",(15,12),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1,cv2.LINE_AA)
+        cv2.putText(image,str(counter),(10,60),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2,cv2.LINE_AA)
+        # Stage data
+        cv2.putText(image, 'STAGE', (65,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, 
+                    (60,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
+
+
+        # Reder detections
+        mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(244,119,64),thickness=2,circle_radius = 2),mp_drawing.DrawingSpec(color=(245,65,232),thickness=2,circle_radius = 2))
+
+        cv2.imshow('Mediapipe Feed', image)
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    # Printing the landmarks - parts of the body it can detect
+    # for connection in mp_pose.PoseLandmark:
+    #     print(connection)
+    # print("Co-ordinates of the Left Wrist")
+    # print(landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+    # print("Co-ordinates of the Left Shoulder")
+    # print(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value])
+        
+    cap.release()
+    cv2.destroyAllWindows()
+    # print(len(landmarks))
+# 25 - left_knee, 26 - right_knee
